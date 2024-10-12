427411084fc2cd6f87f3d927ec8fd4a11135a001
голубев дима <maska.l.v@mail.ru>
2024-10-12 15:07:36 +0500

    7

--- Tyuiu.GolubevDS.Sprint2.Task7.V4.Lib/DataService.cs
+++ Tyuiu.GolubevDS.Sprint2.Task7.V4.Lib/DataService.cs
@@ -6,7 +6,7 @@ public class DataService : ISprint2Task7V4
     public bool CheckDotInShadedArea(double x, double y)
     {
         bool res;
-        if ((x >= -1) && (x < 0) && (x >= 0.3) && (x <= 1) && (y > 0) && (y < 0.3) && (y > 0.3) && (y < 1))
+        if  ((x >= 0.3) && (x <= 1) &&  (y >= 0.3)  && (y <= 1))
         {
             res = true;
 
